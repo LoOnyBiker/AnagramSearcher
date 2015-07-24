e1eb2d51d47a67386a2e0eaaf6e25f7a25faaf15
LoOny <loonybiker@yandex.ru>
2015-07-24 21:55:13 +0300

    Prototype num1

---
+++ DictionaryCreator.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+
+namespace AnagramSearcher
+{
+    public class DictionaryCreator
+    {
+        char[] engChars = new char[26] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        char[] rusChars = new char[29] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ш', 'щ', 'ъ', 'ь', 'э', 'ю', 'я' };
+
+        private System.Random rGen;
+
+        public string GenerateEng(int i)
+        {
+            rGen = new System.Random();
+
+            int p = 0;
+            string str = "";
+            for (int x = 0; x <= i; x++)
+            {
+                p = rGen.Next(0, 25);
+                str += engChars[p];
+            }
+            Thread.Sleep(12);
+            return str;
+        }
+
+        public string GenerateRus(int i)
+        {
+            rGen = new System.Random();
+
+            int p = 0;
+            string str = "";
+            for (int x = 0; x <= i; x++)
+            {
+                p = rGen.Next(0, 28);
+                str += rusChars[p];
+            }
+            Thread.Sleep(12);
+            return str;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,9 +6,18 @@
         {
             string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
+            //AnagramKeeper ak;
+            //ak.unloadFilePath = path + "\\input.txt";
+            //for (int i = 5; i < 15; i++)
+            //{
+            //    for (int j = 0; j < 1000000; j++)
+            //    {
+            //        ak.saveAnagram(new DictionaryCreator().GenerateEng(i));
+            //    }
+            //}
+
             Searcher s = new Searcher(path);
             s.Search();
-
         }
     }
 }
